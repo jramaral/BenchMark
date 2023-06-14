470a4beebd9032681685b5a27aad15910ffbbc7b
Jose Roberto <j.robertoamaral@gmail.com>
2023-06-14 19:33:54 -0400

    Initial commit

---
+++ BenchMark/BenchMark/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchMark
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MemoryBenchMarkerDemo>();
+        }
+    }
+
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class MemoryBenchMarkerDemo
+    {
+        private string title = "Sr.", name = "Jose", middleName = "Roberto", lastName = "Amaral";
+        private readonly int NumberOfItems = 2;
+
+        [Benchmark]
+        public string ConcatStringsUsingStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < NumberOfItems; i++) sb.Append($"Hello World! {i}");
+
+            return sb.ToString();
+        }
+
+        [Benchmark]
+        public string ConcatStringsUsingGenericList()
+        {
+            var list = new List<string>(NumberOfItems);
+            for (var i = 0; i < NumberOfItems; i++) list.Add($"Hello World! {i}");
+
+            return list.ToString();
+        }
+        
+        [Benchmark]
+        public string StringBuilder()
+        {
+            var stringBuilder = new StringBuilder();
+            return stringBuilder
+                .Append(title).Append(' ')
+                .Append(name).Append(' ')
+                .Append(middleName).Append(' ')
+                .Append(lastName).Append(' ').ToString();
+        }
+        [Benchmark]
+        public string StringBuilderExact24()
+        {
+            var stringBuilder = new StringBuilder(24);
+            return stringBuilder
+                .Append(title).Append(' ')
+                .Append(name).Append(' ')
+                .Append(middleName).Append(' ')
+                .Append(lastName).Append(' ').ToString();
+        }
+        [Benchmark]
+        public string StringBuilderEstimate100()
+        {
+            var stringBuilder = new StringBuilder(100);
+            return stringBuilder
+                .Append(title).Append(' ')
+                .Append(name).Append(' ')
+                .Append(middleName).Append(' ')
+                .Append(lastName).Append(' ').ToString();
+        }
+        [Benchmark]
+        public string StringPlus()
+        {
+            return title + ' '+ name +' '+middleName+' '+ lastName;
+            
+        }
+        [Benchmark]
+        public string StringFormat()
+        {
+            return string.Format("{0} {1} {2} {3}", title, name, middleName, lastName); 
+        }
+        [Benchmark]
+        public string StringInterpolation()
+        {
+            return $"{title} {name} {middleName} {lastName}";
+        }
+        [Benchmark]
+        public string StringJoin()
+        {
+            return string.Join(' ', title, name, middleName, lastName); 
+        }
+        [Benchmark]
+        public string StringConcat()
+        {
+            return string.Concat(new string[]{title," ", name," ",middleName," ",lastName}); 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BenchMark
+ performace de strings  .net 5 .net 6 .net 7
